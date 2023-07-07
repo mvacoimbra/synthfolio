0663a24da1cff13b9dfd86d62c93d0c0caa76c5d
mvacoimbra <mv27.marcosvinicius@gmail.com>
2023-07-07 12:58:36 -0300

    new project definition

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
 // sections
-import NavBar from './components/NavBar'
 import Banner from './components/sections/Banner';
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
       <Banner />
     </div>
   );
---
+++ src/components/desktop/Desktop.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// css
+import './Desktop.css';
+// components
+
+const Desktop = () => {
+  return (
+    <div>
+    </div>
+  );
+};
+
+export default Desktop;
